433bd93c72d9dcd2cf9da8ccbe5bb563af6cba31
Fredrik Aarvold <fredrik@digidevs.no>
2026-03-09 10:46:57 +0100

    further expo setup

---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import * as SecureStore from 'expo-secure-store'
+import { Slot } from 'expo-router'
+import '../global.css'
+
+const tokenCache = {
+  async getToken(key: string) {
+    return SecureStore.getItemAsync(key)
+  },
+  async saveToken(key: string, value: string) {
+    return SecureStore.setItemAsync(key, value)
+  },
+}
+
+export default function RootLayout() {
+  return (
+    
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
+}
